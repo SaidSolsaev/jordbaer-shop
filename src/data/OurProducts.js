71ba0ec0547ce3bf4d69e1bec4b137a172c52876
Said Solsaev <said095@hotmail.com>
2023-04-28 20:25:21 +0200

    Initial commit

---
+++ src/data/OurProducts.js
@@ -0,0 +1,29 @@
+import jb from "../Images/jb.jpg"
+
+export const Poducts = [
+    {
+        title: "Jordbær Saga", price: 89.00, pic: {jb},
+    },
+    {
+        title: "Jordbær Korona", price: 99.00, pic: {jb},
+    },
+    {
+        title: "Jordbær Saga", price: 89.00, pic: {jb},
+    },
+    {
+        title: "Svenske Jordbær", price: 69.00, pic: {jb},
+    },
+    {
+        title: "Norske Bringebær", price: 199.00, pic: {jb},
+    },
+    {
+        title: "Tyrkiske Moreller 500g", price: 99.00, pic: {jb},
+    },
+    {
+        title: "Tyrkiske Moreller 750g", price: 129.00, pic: {jb},
+    },
+    {
+        title: "Bringebær", price: 49.00, pic: {jb},
+    },
+
+]
